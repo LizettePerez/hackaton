493e2ea9f3e3c3524b685ff6796858db7b17d8ce
PaulaFG <paulaflores30@gmail.com>
2023-11-14 19:45:41 -0300

    query-rut

--- src/main/java/com/example/hackaton/repositories/ClienteRepository.java
+++ src/main/java/com/example/hackaton/repositories/ClienteRepository.java
@@ -1,4 +1,18 @@
 package com.example.hackaton.repositories;
 
-public interface ClienteRepository {
+import com.example.hackaton.models.Cliente;
+import com.example.hackaton.models.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+
+    @Query("SELECT c FROM Cliente c WHERE c.rut = :rut")
+    List<Cliente> findByRut(@Param("rut") String rut);
+
+
 }
